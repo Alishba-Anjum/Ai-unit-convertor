09bd2eabf2b30a66ca4da19f08a882413e2c5789
Alishba-Anjum <alishbaahmedkhan11@gmail.com>
2025-03-05 01:18:12 +0500

    commit

---
+++ app.py
@@ -0,0 +1,53 @@
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+import streamlit as st
+
+
+load_dotenv()
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+generation_config = {
+    "temperature": 0.1,
+    "top_p": 0.95,
+    "top_k": 40,
+    "max_output_tokens": 256,
+    "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+    model_name="gemini-2.0-flash",
+    generation_config=generation_config,
+    system_instruction="You are a unit conversion tool. Only respond to unit conversion requests. And always answer in table form even if user asks not to or in another form. If a request is not a unit conversion, say 'I can only perform unit conversions.' And if the request is not between valid units say 'Cannot convert between these units.' and tell the reason."
+)
+
+st.title('🔁Unit convertor AI')
+
+
+if 'history' not in st.session_state:
+    st.session_state['history'] = []
+    intro_message = "Hello! I am your unit conversion assistant. Ask me to convert units, and I'll do my best to help. For example, you can ask me to convert kilometers to meters."
+    st.session_state['history'].append({'role': 'assistant', 'parts': [intro_message]})
+
+user_input = st.chat_input("Enter a prompt")
+
+if user_input:
+    st.session_state.history.append({"role": "user", "parts": [user_input]})
+
+    chat_session = model.start_chat(
+    history=st.session_state.history
+    )
+    
+
+    prompt_prefix = "You are a unit conversion tool. Only respond to unit conversion requests. And always answer in table form even if user asks not to or in another form. If a request is not a unit conversion, say 'I can only perform unit conversions.'\n\n"
+    response = chat_session.send_message(prompt_prefix + user_input)
+
+   
+    response = chat_session.send_message(user_input)
+
+    st.session_state.history.append({"role": "assistant", "parts": [response.text]})
+
+for message in st.session_state.history:
+    role = message["role"]
+    content = message["parts"][0]
+    st.chat_message(role).write(content)
